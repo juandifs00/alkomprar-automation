7fc13e2c70eb4f938a1ef1a9789c0f0a6c66796e
Jose Quiroz <jose.quiroz.serna@gmail.com>
2022-10-18 20:31:25 -0500

    Update FilterItemStepDefinitions.java

--- src/test/java/com/alkomprar/stepdefinitions/FilterItemStepDefinitions.java
+++ src/test/java/com/alkomprar/stepdefinitions/FilterItemStepDefinitions.java
@@ -16,8 +16,12 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 public class FilterItemStepDefinitions {
 
     @Cuando("{string} quiere encontrar {string} {string} con {string} de {string}")
-    public void quiereEncontrarConDe(String string, String string2, String string3, String string4, String string5) {
+    public void quiereEncontrarConDe(String actor, String categoria, String marca, String valorCaracteristica, String caracteristica) {
         // Write code here that turns the phrase above into concrete actions
+        theActorCalled(actor).attemptsTo(
+            Open.url("https://www.alkosto.com/"),
+
+        );
         throw new io.cucumber.java.PendingException();
     }
     @Entonces("debe ver la lista hasta con {string} items")
@@ -48,40 +52,6 @@ public class FilterItemStepDefinitions {
     public void debeVerLosHorariosDeAtencion() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
-    }
-
-
-    @Cuando("{string} quiere comprar un articulo de las categorias {string}, {string}, {string}  y {string}")
-    public void rodolfoQuiereComprarUnCelularComputadorTvYLlanta(String actor, String categoria1, String categoria2, String categoria3, String categoria4) {
-        theActorCalled(actor).attemptsTo(
-                Open.url("https://www.alkosto.com/"),
-                AgregarAlCarrito.elemento(categoria1),
-                AgregarAlCarrito.elemento(categoria2),
-                AgregarAlCarrito.elemento(categoria3),
-                AgregarAlCarrito.elemento(categoria4)
-        );
-    }
-    @Entonces("debe ver {int} articulos en el carrito")
-    public void debeVerEnElCarrito(int cantidadArticulos) {
-        theActorInTheSpotlight().attemptsTo(
-                Click.on(CARRITO),
-                Ensure.that(ARTICULOS_CARRITO).values().hasSize(cantidadArticulos)
-        );
-    }
-
-    @Cuando("{string} quiere comprar un {string}")
-    public void rodolfoQuiereComprarUnIphone(String actor, String item) {
-        theActorCalled(actor).attemptsTo(
-                Open.url("https://www.alkosto.com/"),
-                BuscarYAgregarCarrito.elemento(item)
-        );
-    }
-    @Entonces("debe ver en el carrito de comprar {string}")
-    public void debeVerEnElCarritoDeComprarIphone(String item) {
-        theActorInTheSpotlight().attemptsTo(
-                Click.on(CARRITO.of(item)),
-                Ensure.that(ARTICULOS_CARRITO).values().hasSize(1)
-        );
     }
 
 }
